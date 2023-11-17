86458483ef2e5c706ead9b192baa6871c8e8b53a
jose luis montenegro <jose.montenegro.romero@gmail.com>
2023-11-17 16:28:35 -0500

    changes.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
-
 import { routes } from './app.routes';
+// Interceptors
+import { authInterceptor } from '@interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
     provideClientHydration()
   ]
 };
